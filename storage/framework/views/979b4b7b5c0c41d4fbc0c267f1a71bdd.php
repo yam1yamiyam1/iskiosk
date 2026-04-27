612a034d35207f03270db67d8734ab1a0f486804
Yuan Marquez <119801002+yam1yamiyam1@users.noreply.github.com>
2026-04-28 05:09:14 +0800

    sto

--- storage/framework/views/979b4b7b5c0c41d4fbc0c267f1a71bdd.php
+++ storage/framework/views/979b4b7b5c0c41d4fbc0c267f1a71bdd.php
@@ -1,23 +1,61 @@
-<h3>Document Status Update</h3>
+<p>Dear <?php echo e($document->given_name); ?>,</p>
 
-<p>Dear <?php echo e($document->surname); ?>,</p>
+<?php if($document->status === 'Submitted'): ?>
+<p>We have successfully received your document. It has been queued<br>
+for processing and you will be notified once there are updates.</p>
 
-<?php
-    $statusMessage = match($document->status) {
-        'Submitted' => 'We have received your document and it is now submitted for processing.',
-        'Collected and Processing' => 'Your document is currently being processed.',
-        'Ready for claiming' => 'Your document is now ready for claiming. Please visit the office to collect it at your earliest convenience.',
-        'Claimed' => 'Your document has been successfully claimed. Thank you for your cooperation.',
-        default => 'The status of your document has been updated.',
-    };
-?>
+<p>Tracking Code: <?php echo e($document->tracking_code); ?></p>
 
-<p><?php echo e($statusMessage); ?></p>
+<p>Thank you. Please retain your tracking code for future reference.</p>
+
+<p>IsKiosk Document Management System</p>
+
+<?php elseif($document->status === 'Collected and Processing'): ?>
+<p>Your document is currently being reviewed and processed.<br>
+We will send you another update once it is ready for pickup.</p>
+
+<p>Tracking Code: <?php echo e($document->tracking_code); ?></p>
+
+<p>Thank you. Please retain your tracking code for future reference.</p>
+
+<p>IsKiosk Document Management System</p>
+
+<?php elseif($document->status === 'Ready for claiming'): ?>
+<p>Your document is now ready for claiming. Please visit our office<br>
+to collect it at your earliest convenience.</p>
+
+<p>Please bring a valid ID upon claiming.</p>
 
 <?php if($document->remarks): ?>
-<p><strong>Remarks:</strong> <?php echo e($document->remarks); ?></p>
+<p>Remarks: <?php echo e($document->remarks); ?></p>
 <?php endif; ?>
 
-<p><strong>Tracking Code:</strong> <?php echo e($document->tracking_code); ?></p>
+<p>Tracking Code: <?php echo e($document->tracking_code); ?></p>
 
-<p>Thank you. Please retain your tracking code for future reference.</p><?php /**PATH C:\laragon\www\iskiosk\resources\views/emails/document_status_updated.blade.php ENDPATH**/ ?>
+<p>Thank you. Please retain your tracking code for future reference.</p>
+
+<p>IsKiosk Document Management System</p>
+
+<?php elseif($document->status === 'Claimed'): ?>
+<p>Your document has been successfully claimed.<br>
+We hope we were able to assist you.</p>
+
+<p>Tracking Code: <?php echo e($document->tracking_code); ?></p>
+
+<p>Thank you for your cooperation.</p>
+
+<p>IsKiosk Document Management System</p>
+
+<?php else: ?>
+<p>The status of your document has been updated.</p>
+
+<?php if($document->remarks): ?>
+<p>Remarks: <?php echo e($document->remarks); ?></p>
+<?php endif; ?>
+
+<p>Tracking Code: <?php echo e($document->tracking_code); ?></p>
+
+<p>Thank you. Please retain your tracking code for future reference.</p>
+
+<p>IsKiosk Document Management System</p>
+<?php endif; ?><?php /**PATH C:\laragon\www\iskiosk\resources\views/emails/document_status_updated.blade.php ENDPATH**/ ?>
